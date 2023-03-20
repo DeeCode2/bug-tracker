47c761b0b8b60a987be7c55375c50843923c39d2
DeeCode2 <jellyfrog025@gmail.com>
2023-03-20 14:55:49 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1>Hello!</h1>
+  )
+}
+
+export default App
